61085f9ec3ac43af4dac4f83813ad6f214fc07d7
Tobias Kohler <tobias.kohler@unibe.ch>
2025-12-14 09:56:57 +0100

    styling for projects

---
+++ scripts/projects.js
@@ -0,0 +1,13 @@
+// document.querySelectorAll('.project-card').forEach(card => {
+//     const p = card.querySelector('p');
+
+//     card.addEventListener('mouseenter', () => {
+//         p.style.maxHeight = p.scrollHeight + 'px';
+//         p.style.opacity = '0.85';
+//     });
+
+//     card.addEventListener('mouseleave', () => {
+//         p.style.maxHeight = '0';
+//         p.style.opacity = '0';
+//     });
+// });
